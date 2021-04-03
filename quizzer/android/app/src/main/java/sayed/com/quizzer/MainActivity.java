d8941d1a54271fc87c6401e85f169635fd7ddbe4
alfhad <fahadshah2411@gmail.com>
2021-04-04 00:25:01 +0530

    push

---
+++ quizzer/android/app/src/main/java/sayed/com/quizzer/MainActivity.java
@@ -0,0 +1,6 @@
+package sayed.com.quizzer;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ quizzer/lib/question.dart
@@ -0,0 +1,11 @@
+class Question {
+
+  String questionTxt;
+  bool quesAns;
+
+  Question(String quesTxt, bool ans){
+    questionTxt = quesTxt;
+    quesAns = ans;
+  }
+
+}
